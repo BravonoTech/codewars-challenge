fa1ce2965d980845f27d273dad80db409c33f77b
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-05-08 14:09:37 +0100

    Solving another 8kyu kata

---
+++ 8kyu/get-character-from-ASCII-value.js
@@ -0,0 +1,13 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+
+// MY SOLUTION
+function getChar(c) {
+  return String.fromCharCode(c);
+}
