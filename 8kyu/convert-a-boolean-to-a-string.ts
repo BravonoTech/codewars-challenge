afca128f8bbc49e2afdfd6afc238f33632dc38f5
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-05-18 09:35:41 +0100

    TypeScript Kata solutions accross Kyus

---
+++ 8kyu/convert-a-boolean-to-a-string.ts
@@ -0,0 +1,8 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// MY SOLUTION
+export const booleanToString = (b: boolean): string => {
+  return b == true ? "true" : "false";
+};
