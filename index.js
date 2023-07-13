d4b79cd3c25a25b7b703b370828a7d78f20973af
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-07-13 11:07:59 +0100

    Solving morse code

--- index.js
+++ index.js
@@ -1,50 +1,68 @@
-function topThreeWords(text) {
-  let obj = {};
-  let result = [];
-  let newArr = [];
+decodeMorse = function (morseCode) {
+  const morseCodeDict = {
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
+    R: ".-.",
+    S: "...",
+    T: "-",
+    U: "..-",
+    V: "...-",
+    W: ".--",
+    X: "-..-",
+    Y: "-.--",
+    Z: "--..",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    0: "-----",
+  };
 
-  if (text.length === 0) return [];
+  newArr = morseCode.split("   ");
+  console.log("My new Array", newArr);
+  result = [];
 
-  // turn the string into an array of valid words
-  let myArr = text.toLowerCase().match(/[\w'*]+/g);
-
-  // count word occurence
-  for (let i = 0; i < myArr.length; i++) {
-    if (obj[myArr[i]] === undefined) obj[myArr[i]] = 1;
-    else {
-      obj[myArr[i]] += 1;
+  for (let item of newArr) {
+    if (newArr.indexOf(item) != newArr.length - 1) {
+      item.split(" ").forEach(function (code) {
+        for (let key in morseCodeDict) {
+          if (morseCodeDict[key] === code) {
+            result.push(key);
+          }
+        }
+      });
+      result.push(" ");
+    } else {
+      item.split(" ").forEach(function (code) {
+        for (let key in morseCodeDict) {
+          if (morseCodeDict[key] === code) {
+            result.push(key);
+          }
+        }
+      });
     }
   }
+  return `Hello${result.join("")}Bae`;
+};
 
-  for (let e in obj) newArr.push({ key: e, value: obj[e] });
-  let sorted = newArr.sort((a, b) => {
-    if (a.value > b.value) {
-      return -1;
-    } else if (a.value < b.value) {
-      return 1;
-    }
-    return 0;
-  });
-
-  if (Object.keys(sorted).length < 3) {
-    for (let i = 0; i < 2; i++) {
-      if (sorted[i] !== undefined) {
-        result.push(sorted[i].key);
-      }
-    }
-    return result;
-  }
-
-  for (let i = 0; i < 3; i++) {
-    if (sorted[i] !== undefined) {
-      result.push(sorted[i].key);
-    }
-  }
-  return result;
-}
-
-console.log(topThreeWords(`a a a  b c c c`));
-
-// let objs = { a: 1, b: 2 };
-
-// for (let i in objs) console.log(objs[i]);
+console.log(decodeMorse("   .... . -.--   "));
