a2d59d9343831e754246a44e3379b380b0ff6462
Ahbideen Yusuf <ahbideenyusuf@gamil.com>
2023-12-20 16:52:41 +0100

    Add 8ky solution

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const sequenceSum = (begin, end, step) => {
-  // Understanding the question
-  // I want a function that returns the sum of a sequence of integers.
-  // Begin must not be greater than end
-  // I cannot add end if, when we add step to begin, it doesn't equal end
-  //
-  // How to solve
-  // 1. Return immediately if begin is greater than end
-  // 3. Add step to begin and add step to the result until the result is equals to end
-  // 4. We cannot allow result to be begger than end so we return the smallest sum
+function sumStr(a, b) {
+  if (a.length != 0 && b.length != 0) return String(parseInt(a) + parseInt(b));
 
-  if (begin > end) return 0;
+  if ((a + b).length == 0) return "0";
 
-  let result = 0;
+  return a + b;
+}
 
-  for (let i = begin; i <= end; i = i + step) {
-    console.log(i);
-    result += i;
-  }
-
-  return result;
-};
-
-console.log(sequenceSum(1, 5, 3));
+console.log(Number(""));
